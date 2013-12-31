816291e495bba9e09ea6873dcc00d4dd8657809d
Mike O'Connor <fiveironmikey@yahoo.com>
2013-12-31 11:23:39 -0600

    Adding initial code
    Adding the shell of the core library, as well as the P4.NET library.

---
+++ SyncBot.Core/SyncBot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncBot.Core
+{
+    public static class SyncBot
+    {
+		public delegate void GatherEventHandler(object sender, GatherEventArgs e);
+		public static event GatherEventHandler Gathered;
+
+		public delegate void SyncEventHandler(object sender, SyncEventArgs e);
+		public static event SyncEventHandler Syncing;
+		public static event SyncEventHandler Synced;
+
+		public static event EventHandler Finished;
+    }
+
+	public class GatherEventArgs : EventArgs
+	{
+		// Number of files
+		// Total file size
+	}
+
+	public class SyncEventArgs : EventArgs
+	{
+		// Name of file
+		// Revision?
+		// Size
+	}
+}
